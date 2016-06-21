7e1b8b32ee25126a92c34bc33c5f6f701342cea9
Puneet Oberoi <puneet.oberoi@outlook.com>
2016-06-20 23:43:01 -0400

    added buttons and data values

--- eventapi.js
+++ eventapi.js
@@ -21,10 +21,24 @@ $(document).ready(function(){
     .done(function(response) {
       console.log('done');
       console.log(response);
-    //   // var results = response.data;
-    //   // for (i=0; i<results.length; i++){
-    //   //   var gifDiv = $('<span class="items">').attr('class', 'col-md-4');
-    //   //   var rating = results[i].rating;
+      var eventLength = response.events.event.length;
+      var event = response.events.event;
+      console.log(eventLength);
+      for (i=0; i<eventLength; i++){
+        var infoDiv = $('<button>').attr('class', 'col-md-4');
+        var eventTitle = event[i].title;
+        console.log(eventTitle);
+        var eventCity = event[i].city_name;
+        console.log(eventCity);
+        infoDiv.append(eventCity);
+        infoDiv.append('<br/>');
+        infoDiv.append(eventTitle);
+        infoDiv.attr('data-lat', event[i].latitude);
+        infoDiv.attr('data-lng', event[i].longitude);
+        console.log(infoDiv.data('lat'));
+        console.log(infoDiv.data('lng'));
+        $('#eventsDisplay').append(infoDiv);
+      }
     //   //   var p = $('<p>').text('Rating: ' + rating);
     //   //   var personImage = $('<img>');
     //   //   personImage.attr('src', results[i].images.original_still.url);
